c7c6d87482383666b8824ddbca7fd7faf2877030
Yannick Mounguengui <angeyannick.mounguenguiitoua.etu@univ-lille.fr>
2024-09-29 17:54:48 -0600

    third part done

--- server/database/app.js
+++ server/database/app.js
@@ -59,16 +59,34 @@ app.get('/fetchReviews/dealer/:id', async (req, res) => {
 // Express route to fetch all dealerships
 app.get('/fetchDealers', async (req, res) => {
 //Write your code here
+try {
+  const documents = await Dealerships.find();
+  res.json(documents);
+} catch (error) {
+  res.status(500).json({ error: 'Error fetching dealerships' });
+}
 });
 
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
+try {
+  const documents = await Dealerships.find({ state: req.params.state });
+  res.json(documents);
+} catch (error) {
+  res.status(500).json({ error: 'Error fetching dealerships by state' });
+}
 });
 
 // Express route to fetch dealer by a particular id
 app.get('/fetchDealer/:id', async (req, res) => {
 //Write your code here
+try {
+  const document = await Dealerships.findById(req.params.id);
+  res.json(document);
+} catch (error) {
+  res.status(500).json({ error: 'Error fetching dealership by id' });
+}
 });
 
 //Express route to insert review
