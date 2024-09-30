1622e6d0ac741128e52ada0c41e1d58c4c50d5bb
Yannick Mounguengui <angeyannick.mounguenguiitoua.etu@univ-lille.fr>
2024-09-29 19:03:46 -0600

    fourth part done

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,8 +11,11 @@ urlpatterns = [
     # path for login
      path(route='login', view=views.login_user, name='login'),
      
-     #path for logout
+    #path for logout
      path(route='logout', view=views.logout_request, name='logout'),
+     
+    #path for cars
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 
     # path for dealer reviews view
 
